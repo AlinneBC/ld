ad5a9257be917f8b6868a59f1ca8a704d7710fb3
AlinneBC <alinne@estudante.ufscar.br>
2025-05-06 08:41:30 -0300

    Update top.v

--- labs/05_halfadder/top.v
+++ labs/05_halfadder/top.v
@@ -3,5 +3,12 @@ module top(
   output [6:0] HEX0); // a, b, c, d, e, f, g
   // instancie e conecte os módulos a seguir
 
+  assign HEX0[6] = ~(SW1] ^ SW[0]);
+  assign HEX0[5] = 1'b1;
+  assign HEX0[4] = ~(SW[1] & SW[0]);
+  assign HEX0[3] = ~(SW[1] ^ SW[0]);
+  assign HEX0[2] = ~(SW[1] ^ SW[0]);
+  assign HEX0[1] = ~(SW[1] ^ SW[0]) & ~(SW[1] & SW[0]);
+  assign HEX0[0] = ~(SW[1] ^ SW[0]) & (SW[1] & SW[0]);
 
 endmodule
